2dfcf2670e92a8aa6aec0d4ec07f94fcfe7dddff
Bhaskar <reddybhaskar437@gmail.com>
2021-01-27 22:24:34 -0600

    changed locations

--- init-location.js
+++ init-location.js
@@ -1,22 +1,24 @@
+// export default object with the initialization data
+
 export default [
     {
-      Name: 'Horizons',
-      Latitude: 40.3589695,
-      Longitude: -94.8831951,
-    //   South: 40.3589695,
-    //   East: -94.8831951,
-    //   Notes: 'circle',
-    //   Attachments: '',
-    //   'Quest-Location': 'N college dr One',
-    },
-    {
-      Name: 'Faustiana',
-      North: 40.343579,
-      West: -94.89084,
-      South: 40.343268,
-      East: -94.8884,
-    //   Notes: 'heading south',
-    //   Attachments: '',
-    //   'Quest-Location': 'Faustiana-Faustiana',
-    },
+  Name: 'Horizons west apartments',
+  //  Latitude: 40.35614, 
+  //  Longitude: -94.88189,
+  // North: 40.35614, 
+  // South: 40.35614, 
+  // East: -94.88189,
+  // West:-94.88189,
+  //  South: 35.8425,
+},
+     {
+  Name: 'mooyah',
+  Latitude:40.35287,
+  Longitude:-98.88805,
+  North: 40.35287, 
+  West: -98.88805,
+  South: 40.35287, 
+  East: -98.88805,
+}
+
 ];
