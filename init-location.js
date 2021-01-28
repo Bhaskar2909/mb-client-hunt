f316e67a2282a985fc03705a265543356597f47c
Bhaskar <reddybhaskar437@gmail.com>
2021-01-27 21:49:33 -0600

    Added main.js and locations.

---
+++ init-location.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      Name: 'Horizons',
+      North: 40.3589695,
+      West: -94.8831951,
+      South: 40.3589695,
+      East: -94.8831951,
+      Notes: 'circle',
+      Attachments: '',
+    //   'Quest-Location': 'N college dr One',
+    },
+    {
+      Name: 'Faustiana',
+      North: 40.343579,
+      West: -94.89084,
+      South: 40.343268,
+      East: -94.8884,
+      Notes: 'heading south',
+      Attachments: '',
+      'Quest-Location': 'Faustiana-Faustiana',
+    },
+];
